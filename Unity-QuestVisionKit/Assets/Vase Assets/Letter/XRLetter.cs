7e2c62831417447c63ec1e2c9fb720bf2c0df50e
Felix Jarquin <fjarq005@fiu.edu>
2025-07-07 23:10:09 -0400

    Canvas PopUp Layout

--- Unity-QuestVisionKit/Assets/Vase Assets/Letter/XRLetter.cs
+++ Unity-QuestVisionKit/Assets/Vase Assets/Letter/XRLetter.cs
@@ -4,17 +4,35 @@ using UnityEngine;
 public class XRLetter : MonoBehaviour
 {
     [SerializeField] private GameObject uiMenu;
+    [SerializeField] private AnimationClip letterAnimation;
+    
+    private Animator letterAnimator;
     
     void Start()
     {
         uiMenu.SetActive(false);
-        
-        GetComponentInChildren<HandGrabInteractable>().WhenSelectingInteractorAdded.Action += ShowUI;
+        letterAnimator = GetComponent<Animator>();
     }
     
-    private void ShowUI(HandGrabInteractor interactor)
+    void Update()
+    {
+        if (letterAnimator.GetCurrentAnimatorStateInfo(0).IsName(letterAnimation.name))
+        {
+            if (letterAnimator.GetCurrentAnimatorStateInfo(0).normalizedTime >= 0.95f)
+            {
+                Invoke(nameof(ShowUI), 3f);
+                this.enabled = false;
+            }
+        }
+    }
+    
+    private void ShowUI()
     {
         uiMenu.SetActive(true);
-        uiMenu.GetComponent<Animator>().Play("UIBounce");
+        
+        if (uiMenu.GetComponent<Animator>() != null)
+        {
+            uiMenu.GetComponent<Animator>().Play("UIBounce");
+        }
     }
 }
